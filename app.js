bd667260cb56874b37e5901fced4985255715327
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2022-06-01 12:21:02 -0400

    Lat/Lon coordinates
    Here we created functions that will allow us to use API calls to get the weather forecast for a location based on it's coordinates.

--- app.js
+++ app.js
@@ -20,10 +20,9 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
-
-  //we need to CAREFULLy populate this var with the right content for the forecast
   let forecastHTML = `<div class = "row">`;
   let days = ["Thu", "Fri", "Sat"];
   days.forEach(function (day) {
@@ -56,6 +55,13 @@ function displayForecast() {
   //use a loop! make a new var above to store the HTML of the forecast.
 }
 
+function getForecast(coordinates) {
+  let apiKey = "0aa51f2ae72d62c67ab574237edb123f";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
@@ -85,6 +91,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
